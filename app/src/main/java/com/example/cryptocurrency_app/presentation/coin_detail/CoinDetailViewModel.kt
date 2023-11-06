abcea8c34eab40f9275925e78dbc69f3b4fadad3
furkan harmancı <rocklee34@hotmail.com>
2023-11-06 12:21:09 +0300

    feat: viewmodels added :sparkles:

---
+++ app/src/main/java/com/example/cryptocurrency_app/presentation/coin_detail/CoinDetailViewModel.kt
@@ -0,0 +1,49 @@
+package com.example.cryptocurrency_app.presentation.coin_detail
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurrency_app.common.Constants
+import com.example.cryptocurrency_app.common.Resource
+import com.example.cryptocurrency_app.domain.use_case.get_coin.GetCoinUseCase
+import com.example.cryptocurrency_app.domain.use_case.get_coins.GetCoinsUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+class CoinDetailViewModel @Inject constructor(
+    private val getCoinUseCase: GetCoinUseCase,
+    private val savedStateHandle: SavedStateHandle
+): ViewModel() {
+
+    private val _state = mutableStateOf(CoinDetailState())
+    val state: State<CoinDetailState> = _state
+
+    init {
+        savedStateHandle.get<String>(Constants.PARAM_COIN_ID)?.let { coinId ->
+            getCoin(coinId)
+        }
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    private fun getCoin(coinId: String) {
+        getCoinUseCase(coinId).onEach { result ->
+            when(result) {
+                is Resource.Success -> {
+                    _state.value = CoinDetailState(coin = result.data)
+                }
+                is Resource.Error -> {
+                    _state.value = CoinDetailState(error = result.message ?: "An unexpected error occured!")
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinDetailState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
---
+++ app/src/main/java/com/example/cryptocurrency_app/presentation/coin_list/CoinListViewModel.kt
@@ -0,0 +1,43 @@
+package com.example.cryptocurrency_app.presentation.coin_list
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cryptocurrency_app.common.Resource
+import com.example.cryptocurrency_app.domain.use_case.get_coins.GetCoinsUseCase
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import javax.inject.Inject
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+class CoinListViewModel @Inject constructor(
+    private val getCoinsUseCase: GetCoinsUseCase
+): ViewModel() {
+
+    private val _state = mutableStateOf(CoinListState())
+    val state: State<CoinListState> = _state
+
+    init {
+        getCoins()
+    }
+
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    private fun getCoins() {
+        getCoinsUseCase().onEach { result ->
+            when(result) {
+                is Resource.Success -> {
+                    _state.value = CoinListState(coins = result.data ?: emptyList())
+                }
+                is Resource.Error -> {
+                    _state.value = CoinListState(error = result.message ?: "An unexpected error occured!")
+                }
+                is Resource.Loading -> {
+                    _state.value = CoinListState(isLoading = true)
+                }
+            }
+        }.launchIn(viewModelScope)
+    }
+}
