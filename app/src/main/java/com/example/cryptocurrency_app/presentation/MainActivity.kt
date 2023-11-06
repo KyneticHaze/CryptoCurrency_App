e262da392dc22b703ffcbf07f3bef115e4174fdd
furkan harmancı <rocklee34@hotmail.com>
2023-11-06 12:41:41 +0300

    feat: NavHost added :sparkles:

--- app/src/main/java/com/example/cryptocurrency_app/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency_app/presentation/MainActivity.kt
@@ -1,18 +1,23 @@
 package com.example.cryptocurrency_app.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.annotation.RequiresExtension
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptocurrency_app.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptocurrency_app.presentation.coin_list.CoinListScreen
 import com.example.cryptocurrency_app.presentation.ui.theme.CryptoCurrency_AppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -21,7 +26,19 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     color = MaterialTheme.colorScheme.background
                 ) {
+                    val navController = rememberNavController()
 
+                    NavHost(
+                        navController = navController,
+                        startDestination = Screen.CoinListScreen.route
+                    ) {
+                        composable(Screen.CoinListScreen.route) {
+                            CoinListScreen(navController = navController)
+                        }
+                        composable(Screen.CoinDetailScreen.route + "/{coinId}") {
+                            CoinDetailScreen()
+                        }
+                    }
                 }
 
             }
