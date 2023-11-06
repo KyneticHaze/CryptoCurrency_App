b2b24374957e10cdf2a61dd7fdef266451ef7f2f
furkan harmancı <rocklee34@hotmail.com>
2023-11-06 11:23:24 +0300

    feat: usecases added :sparkles:

---
+++ app/src/main/java/com/example/cryptocurrency_app/domain/use_case/get_coin/GetCoinUseCase.kt
@@ -0,0 +1,32 @@
+package com.example.cryptocurrency_app.domain.use_case.get_coin
+
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import com.example.cryptocurrency_app.common.Resource
+import com.example.cryptocurrency_app.data.remote.dto.toCoin
+import com.example.cryptocurrency_app.data.remote.dto.toCoinDetail
+import com.example.cryptocurrency_app.domain.model.Coin
+import com.example.cryptocurrency_app.domain.model.CoinDetail
+import com.example.cryptocurrency_app.domain.repository.CoinRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOException
+import javax.inject.Inject
+
+class GetCoinUseCase @Inject constructor(
+    private val repository: CoinRepository
+) {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    operator fun invoke(coinId: String): Flow<Resource<CoinDetail>> = flow {
+        try {
+            emit(Resource.Loading())
+            val coin = repository.getCoinById(coinId).toCoinDetail()
+            emit(Resource.Success(coin))
+        } catch (e: HttpException) {
+            emit(Resource.Error(e.localizedMessage ?: "Unexpected error occured!"))
+        } catch (e: IOException) {
+            emit(Resource.Error("Couldn't reach server. Check your internet connection"))
+        }
+    }
+}
