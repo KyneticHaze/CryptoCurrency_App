89b4d17e6d11a20d2fccededb6a8c1e4b906657a
furkan harmancı <rocklee34@hotmail.com>
2023-11-06 10:21:19 +0300

    feat: dto classes added :sparkles:

---
+++ app/src/main/java/com/example/cryptocurrency_app/data/remote/dto/CoinDto.kt
@@ -0,0 +1,23 @@
+package com.example.cryptocurrency_app.data.remote.dto
+
+
+import com.example.cryptocurrency_app.domain.model.Coin
+import com.google.gson.annotations.SerializedName
+
+data class CoinDto(
+    val id: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    val name: String,
+    val rank: Int,
+    val symbol: String,
+    val type: String
+)
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id, isActive, name, rank, symbol
+    )
+}
